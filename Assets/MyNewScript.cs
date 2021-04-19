04d007b39f144e174fbce407a127f593d00611f2
Hoooon22 <momo990305@gmail.com>
2021-04-20 03:08:27 +0900

    variable

--- Assets/MyNewScript.cs
+++ Assets/MyNewScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MyNewScript : MonoBehaviour
 {
+    public string PlayerName = "";
+    public int PlayerHealth = 100;
+    public Vector3 Position = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
