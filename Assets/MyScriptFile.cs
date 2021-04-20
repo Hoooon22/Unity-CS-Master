2843d0f77ca58ce00449c0de93c8a249084a160b
Hoooon22 <momo990305@gmail.com>
2021-04-20 19:24:08 +0900

    Array

---
+++ Assets/MyScriptFile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScriptFile : MonoBehaviour
+{
+    // Array of Scene's GameObject
+    public GameObject[] MyObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
