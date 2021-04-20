c3a59fcde36f34cfc85abecb90ce9dfb05fb5f8d
Hoooon22 <momo990305@gmail.com>
2021-04-20 21:51:33 +0900

    Fill in the array

--- Assets/MyScriptFile.cs
+++ Assets/MyScriptFile.cs
@@ -10,7 +10,14 @@ public class MyScriptFile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 배열의 내용을 코드로 채운다
+        MyObjects = new GameObject[3];
+        // 씬에 MainCamera 태그로 설정된 카메라가 있어야 한다
+        MyObjects[0] = Camera.main.gameObject;
+
+        // GameObject.Find 함수를 사용해 씬의 오브젝트를 이름으로 찾는다
+        MyObjects[1] = GameObject.Find("Cube");
+        MyObjects[2] = GameObject.Find("Cylinder");
     }
 
     // Update is called once per frame
