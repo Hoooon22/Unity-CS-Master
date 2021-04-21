3d7c505f0acb18c0b42cb43e0dc9a41c8a1c8631
Hoooon22 <momo990305@gmail.com>
2021-04-21 19:40:22 +0900

    ErrorScript.cs

--- Assets/ErrorScript.cs
+++ Assets/ErrorScript.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ErrorScript : MonoBehaviour
 {
+    int MyNumber = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mynumber = 7;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mynumber = 10;    
     }
 }
