f5919f39e136e164779f0efd7a88b6ef17e7df0a
Hoooon22 <momo990305@gmail.com>
2021-04-27 16:32:48 +0900

    create Parenter.cs

---
+++ Assets/Scripts/3_GameObject/Parenter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parenter : MonoBehaviour
+{
+    // 씬의 자식 오브젝트에 대한 참조
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
